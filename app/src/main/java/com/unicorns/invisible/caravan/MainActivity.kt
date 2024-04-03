72c2d16657454a4571566ab2a6540bf1cc390329
subject-name-here <jura.paradov@gmail.com>
2024-04-03 19:42:36 +0300

    more things done

--- app/src/main/java/com/unicorns/invisible/caravan/MainActivity.kt
+++ app/src/main/java/com/unicorns/invisible/caravan/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.unicorns.invisible.caravan.model.CardBack
 
 
@@ -27,29 +35,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
     @Composable
     fun MainMenu() {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
             Text(
                 text = "PvAI",
                 modifier = Modifier.clickable {
 
                 }
             )
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "PvE",
                 modifier = Modifier.clickable {
 
                 }
             )
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "PvP",
                 modifier = Modifier.clickable {
 
                 }
             )
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "Deck",
                 modifier = Modifier.clickable {
