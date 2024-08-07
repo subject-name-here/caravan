8476e537837455d82cb592ea35588f32e6ab1590
subject-name-here <jura.paradov@gmail.com>
2024-08-07 11:12:23 +0300

    finale.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.unicorns.invisible.caravan"
         minSdk = 23
         targetSdk = 34
-        versionCode = 54
-        versionName = "1.6.L"
+        versionCode = 55
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
